74444b40850169f4a7c4f7a33d644682e2148f74
fdicera <67300276+fdicera@users.noreply.github.com>
2023-10-23 22:06:21 +0000

    Add square results pages.

--- app.rb
+++ app.rb
@@ -3,7 +3,14 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
+  <h1>Random Results</h1>
   "
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  erb(:square_results)
 end
